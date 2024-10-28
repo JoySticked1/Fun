6bee042328d3ce16715ec116648cac3321fde094
JoySticked1 <DavidOliverM10@gmail.com>
2024-10-28 17:20:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var	readyToJump = false
+var countdown = 10
+function setup() {
+    world.gravity.y = 10;
+	player = new Sprite()
+	player.bounciness = 0
+	player.d = 60
+  player.x = windowWidth/2
+	player.y = windowHeight/2
+	createCanvas(windowWidth, windowHeight);
+	ground = new Sprite()
+	ground.width = windowWidth*3
+	ground.x = windowWidth/2
+	ground.y = windowHeight - 1
+	ground.collider = "s"
+	ground.bounciness = 0.3
+	background(100)
+}
+function draw() {
+  countdown += 1
+	if(countdown==10){
+		readyToJump = false
+	}
+  background(random(0,25),random(0,25),random(0,25))
+	if (kb.pressing('space') || kb.pressing('up') || kb.pressing('w')) {
+		readyToJump = true
+		countdown = 0
+	}
+	if (kb.pressing('down') || kb.pressing('s')) {
+		player.velocity.y += 0.1 
+	}
+	if (kb.pressing('right') || kb.pressing('d')) {
+		player.velocity.x += 0.15
+	}
+	if (kb.pressing('left') || kb.pressing('a')) {
+		player.velocity.x -= 0.15
+	}
+	if(player.colliding(ground) > 0){
+		player.velocity.y = 0
+	}
+  if(player.colliding(ground) > 0 && readyToJump == true){
+		player.velocity.y = -10	
+		readyToJump = false
+	}
+}
